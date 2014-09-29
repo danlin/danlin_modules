10a33f2fad0c0ee569f0a3c73c646e4fe3da9e88
Daniel Lindenfelser <daniel@lindenfelser.de>
2014-09-29 06:38:43 +0200

    add font awesome module

---
+++ modules/danlin_fontawesome/data/BinaryData.h
@@ -0,0 +1,10 @@
+#ifndef FONTAWESOMEDATA_INCLUDED
+#define FONTAWESOMEDATA_INCLUDED
+
+namespace FontAwesomeData
+{
+    extern const char*   fontawesomewebfont_ttf;
+    const int            fontawesomewebfont_ttfSize = 141564;
+}
+
+#endif
