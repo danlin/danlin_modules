d21ec74378cf15fcfbe32893a5ff7223eacb1c89
Daniel Lindenfelser <daniel@lindenfelser.de>
2014-11-29 09:03:27 +0100

    include fix

--- modules/danlin_fontawesome/danlin_fontawesome.h
+++ modules/danlin_fontawesome/danlin_fontawesome.h
@@ -1,8 +1,8 @@
 #ifndef __FONTAWESOME_HEADER__
 #define __FONTAWESOME_HEADER__
 
-#include "../juce_core/juce_core.h"
-#include "../juce_graphics/juce_graphics.h"
+#include "juce_core/juce_core.h"
+#include "juce_graphics/juce_graphics.h"
 
 #include "data/BinaryData.h"
 #include "src/FontAwesome.h"
