97274d68fd08ee19d8d3b9205c092e67ba089c0f
Daniel Lindenfelser <daniel@lindenfelser.de>
2014-12-02 06:42:39 +0100

    better error handling

--- modules/danlin_oscserver/src/OscServer.cpp
+++ modules/danlin_oscserver/src/OscServer.cpp
@@ -12,68 +12,97 @@
 
 using namespace danlin;
 
-OscServer::OscServer(OscMessageListener *listener)
-    : Thread("OscServer"), listener(listener) {}
-
-OscServer::~OscServer() {
-  if (serverDatagramSocket) {
-    serverDatagramSocket->close();
-  }
-  if (clientDatagramSocket) {
-    clientDatagramSocket->close();
-  }
-  stopThread(100);
-  serverDatagramSocket = nullptr;
-  clientDatagramSocket = nullptr;
+OscServer::OscServer(OscMessageListener* listener)
+    : Thread("OscServer")
+    , listener(listener)
+{
 }
 
-void OscServer::setLocalPortNumber(int portNumber) {
-  localPortNumber = portNumber;
+OscServer::~OscServer()
+{
+    if (serverDatagramSocket) {
+        serverDatagramSocket->close();
+    }
+    if (clientDatagramSocket) {
+        clientDatagramSocket->close();
+    }
+    stopThread(100);
+    serverDatagramSocket = nullptr;
+    clientDatagramSocket = nullptr;
+}
+
+void OscServer::setLocalPortNumber(int portNumber)
+{
+    localPortNumber = portNumber;
 }
 
 int OscServer::getLocalPortNumber() { return localPortNumber; }
 
-void OscServer::setRemoteHostname(juce::String hostname) {
-  remoteHostname = hostname;
+void OscServer::setRemoteHostname(juce::String hostname)
+{
+    remoteHostname = hostname;
 }
 
 juce::String OscServer::getRemoteHostname() { return remoteHostname; }
 
-void OscServer::setRemotePortNumber(int portNumber) {
-  remotePortNumber = portNumber;
+void OscServer::setRemotePortNumber(int portNumber)
+{
+    remotePortNumber = portNumber;
 }
 
 int OscServer::getRemotePortNumber() { return remotePortNumber; }
 
 bool OscServer::isConnected() { return clientDatagramSocket->isConnected(); }
 
-bool OscServer::connect() {
-  clientDatagramSocket = new juce::DatagramSocket(0);
-  return clientDatagramSocket->connect(remoteHostname, remotePortNumber);
+bool OscServer::connect()
+{
+    clientDatagramSocket = new juce::DatagramSocket(0);
+    return clientDatagramSocket->connect(remoteHostname, remotePortNumber);
 }
 
-void OscServer::listen() { startThread(1); }
-
-void OscServer::run() {
-  serverDatagramSocket = new juce::DatagramSocket(localPortNumber);
-
-  juce::MemoryBlock buffer(bufferSize, true);
-  while (!threadShouldExit()) {
-    if (serverDatagramSocket->waitUntilReady(true, 100)) {
-      int size =
-          serverDatagramSocket->read(buffer.getData(), buffer.getSize(), false);
-      osc::ReceivedPacket packet((const char *)buffer.getData(), size);
-      if (listener != nullptr)
-        listener->postMessage(new ReceivedOscMessage(packet));
+void OscServer::listen()
+{
+    if (isThreadRunning()) {
+        stopThread(100);
+        if (serverDatagramSocket) {
+            serverDatagramSocket->close();
+        }
+        serverDatagramSocket = nullptr;
     }
-  }
+    startThread(1);
 }
 
-bool OscServer::sendMessage(osc::OutboundPacketStream stream) {
-  if (clientDatagramSocket->isConnected()) {
-    if (clientDatagramSocket->write(stream.Data(), stream.Size()) > 0) {
-      return true;
+void OscServer::run()
+{
+    serverDatagramSocket = new juce::DatagramSocket(localPortNumber);
+
+    juce::MemoryBlock buffer(bufferSize, true);
+    while (!threadShouldExit()) {
+        if (serverDatagramSocket->waitUntilReady(true, 100)) {
+            int size = serverDatagramSocket->read(buffer.getData(), buffer.getSize(), false);
+            try {
+                osc::ReceivedPacket packet((const char*)buffer.getData(), size);
+                if (listener != nullptr)
+                    listener->postMessage(new ReceivedOscMessage(packet));
+            }
+            catch (osc::Exception& e) {
+                if (serverDatagramSocket->isConnected()) {
+                    std::cout << "disconnected" << std::endl;
+                }
+                std::cout << "error while parsing packet" << std::endl;
+            }
+        }
     }
-  }
-  return false;
+}
+
+bool OscServer::sendMessage(osc::OutboundPacketStream stream)
+{
+    if (clientDatagramSocket) {
+        if (clientDatagramSocket->isConnected()) {
+            if (clientDatagramSocket->write(stream.Data(), stream.Size()) > 0) {
+                return true;
+            }
+        }
+    }
+    return false;
 }
