dbf51fc3c59d2a4349ebedf86d1455efed0856aa
Daniel Lindenfelser <daniel@lindenfelser.de>
2016-10-25 15:01:50 +0200

    update module format

--- modules/danlin_oscpack/danlin_oscpack.h
+++ modules/danlin_oscpack/danlin_oscpack.h
@@ -1,6 +1,29 @@
 #ifndef __DANLIN_OSCPACK_HEADER__
 #define __DANLIN_OSCPACK_HEADER__
 
+/*******************************************************************************
+ The block below describes the properties of this module, and is read by
+ the Projucer to automatically generate project code that uses it.
+ For details about the syntax and how to create or use a module, see the
+ JUCE Module Format.txt file.
+
+
+ BEGIN_JUCE_MODULE_DECLARATION
+
+  ID:               danlin_oscpack
+  vendor:           danlin
+  version:          1.1.0
+  name:             Oscpack
+  description:      Open Sound Control (OSC) packet manipulation library.
+  website:          https://github.com/danlin/danlin_modules
+  license:          The MIT License (MIT)
+
+  dependencies:     juce_core
+
+ END_JUCE_MODULE_DECLARATION
+
+*******************************************************************************/
+
 #include "juce_core/juce_core.h"
 
 #include "src/OscTypes.h"
--- modules/danlin_oscserver/danlin_oscserver.h
+++ modules/danlin_oscserver/danlin_oscserver.h
@@ -1,6 +1,29 @@
 #ifndef __DANLIN_OSCSERVER_HEADER__
 #define __DANLIN_OSCSERVER_HEADER__
 
+/*******************************************************************************
+ The block below describes the properties of this module, and is read by
+ the Projucer to automatically generate project code that uses it.
+ For details about the syntax and how to create or use a module, see the
+ JUCE Module Format.txt file.
+
+
+ BEGIN_JUCE_MODULE_DECLARATION
+
+  ID:               danlin_oscserver
+  vendor:           danlin
+  version:          0.8.0
+  name:             Simple Osc Server
+  description:      Simple Osc Server.
+  website:          https://github.com/danlin/danlin_modules
+  license:          The MIT License (MIT)
+
+  dependencies:     juce_core juce_events danlin_oscpack
+
+ END_JUCE_MODULE_DECLARATION
+
+*******************************************************************************/
+
 #include "juce_core/juce_core.h"
 #include "juce_events/juce_events.h"
 
